d78cfd5a1673acb71e64317647e025f8a57542a4
comishOmar <comish2009x@live.com>
2017-08-29 12:31:22 +0200

    Inital commit

---
+++ index.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+"use strict"
+
+var client = require("node-rest-client").Client;
+var fs = require("fs");
+
+//fs.writeFileSync("./data/person.sql", "[\n");
+
+if (fs.existsSync("./data/person.sql")) {
+
+    fs.writeFileSync("./data/person.sql", "insert into person(");
+
+}
+
+(new client()).get("http://localhost:5984/ccc/_design/Person/_view/obs_encounters_only?keys=" +
+    encodeURIComponent(JSON.stringify(knownEncounters)) + "&include_docs=true&reduce=false", function (data) {
+        
+}).on('error', function (err) {
+    console.log(err.message, err.request.options);
+});
+
+fs.appendFileSync("./data/person.sql", ");");
